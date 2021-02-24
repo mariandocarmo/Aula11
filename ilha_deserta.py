48a040f0fef86ea06a7877f872fc989a03eafbea
Mariãn do Carmo <marianlimadocarmo@gmail.com>
2021-02-24 14:40:27 -0300

    Inserindo código de python

---
+++ ilha_deserta.py
@@ -0,0 +1,4 @@
+primeiro_item = input("Quais o primeiro iten que você levaria para uma ilha deserta?")
+segundo_item = input("Qual o segundo item que você levaria para uma ilha deserta?")
+terceiro_item = input("Qual o terceiro item que você levaria para uma ilha deserta?")
+print (f"Você levaria para uma ilha deserta, {primeiro_item}, {segundo_item}, {terceiro_item}")
